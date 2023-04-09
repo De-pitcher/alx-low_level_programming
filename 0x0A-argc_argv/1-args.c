e49021ebd078a880a7ce0fd07ac62632a623d33b
De-pitcher <emmanwa000@gmail.com>
2023-04-09 11:28:36 -0700

    Fixed betty error

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * mai - print the number of arguments passed to the program
+ * main - print the number of arguments passed to the program
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: Always 0 (Success)
