d0969877fe6fd01873ec2bde1d14013a66bb99b8
De-pitcher <emmanwa000@gmail.com>
2023-05-02 07:57:51 -0700

    Created function prints all elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,39 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked node structure
+ * for Holberton project
+ */
+
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/**
+ * struct list_addr_s - singly linked list
+ * @addr: pointer address value
+ * @next: points to the next node
+ *
+ * Description: This is a singly linked node
+ * structure
+ */
+
+typedef struct list_addr_s
+{
+    size_t addr;
+    struct list_addr_s *next;
+} list_addr;
+
+
+size_t print_listint(const listint_t *h);
+#endif
