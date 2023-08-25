860891c3eb60657aed5ebcb75ed6ce4e78a14f47
Noel Emmanuel <emmanwa000@gmail.com>
2023-08-25 15:47:59 -0700

    Let\'s call C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+
+/**
+ * add - add two int.
+ * @a: first int
+ * @b: second int
+ * Return: sum of the two int.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two int.
+ * @a: first int
+ * @b: second int
+ * Return: difference between two int.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul two int.
+ * @a: first int
+ * @b: second int
+ * Return: mul of the two int.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div two int.
+ * @a: first int
+ * @b: second int
+ * Return: div of the two int.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of two int.
+ * @a: first int
+ * @b: second int
+ * Return: modulus of two int.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
